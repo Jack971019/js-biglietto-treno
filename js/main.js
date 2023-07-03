46a65710cdec2af34afa96a5f791efac230ed9c4
Giacomotestori <giacomo.testori10@gmail.com>
2023-07-03 16:05:17 +0200

    final price with variables

--- js/main.js
+++ js/main.js
@@ -7,25 +7,24 @@ let age = prompt("insersici la tua età");
 //calcoli 
 
 let kmprice = kmnumbers * 0.21
-let priceunder18 = kmprice - kmprice * 20 / 100 
+let priceunder18 = kmprice - kmprice * 20 / 100
 let priceover65 = kmprice - kmprice * 40 / 100
 
-//console
 
 
 
 //variabili booleane 
 
-if( age < 18){
-    console.log("il tuo biglietto costa" + " " + priceunder18)
-} else if(age > 65){
-    console.log("il tuo biglietto costa" + " " + priceover65)
-} else{
-    console.log("il tuo biglietto costa" + " " + kmprice)
+if (console.log < 18) {
+    document.getElementById("final_price").innerHTML = console.log("il tuo biglietto costa" + " " + priceunder18)
+}
+if (age > 65) {
+    document.getElementById("final_price").innerHTML = console.log("il tuo biglietto costa" + " " + priceover65)
+} else {
+    document.getElementById("final_price").innerHTML = console.log("il tuo biglietto costa" + " " + kmprice)
 }
 
 //stampa in html
 
-document.getElementById("final_price").innerHTML = kmprice;
 
 
